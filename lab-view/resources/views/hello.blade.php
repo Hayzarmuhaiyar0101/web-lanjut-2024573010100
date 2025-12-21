ab0f7b74b1666366d5d9d1cd9afa13b4d4145e5c
Hayzar <hayzarmuhaiyar@gmail.com>
2025-12-21 12:38:29 +0700

    update views

--- lab-view/resources/views/hello.blade.php
+++ lab-view/resources/views/hello.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello</title>
+    <title>Holla</title>
 </head>
 <body>
     <h1> Hello, {{ $name }}!</h1>
